c75a6605d3ac43d9f891eed0517c9f3c49e29e2a
Mani <deepanshukumar1542004@gmail.com>
2025-04-07 11:08:21 +0530

    update files

--- ajanta/src/components/Core/Homepage/Banner.tsx
+++ ajanta/src/components/Core/Homepage/Banner.tsx
@@ -9,20 +9,20 @@ const Banner: React.FC = () => {
       <Image
         src={bannerImg1}
         alt="Banner Background"
-        className="w-full h-[50vh] md:h-[70vh] lg:h-[90vh] object-cover object-center"
+        className="w-full h-[40vh] sm:h-[50vh] md:h-[70vh] lg:h-[90vh] object-cover object-center"
         priority
       />
 
       {/* Text Content */}
-      <div className="absolute inset-0 flex flex-col justify-center items-start px-4 sm:px-6 md:px-20">
-        <div>
+      <div className="absolute inset-0 flex flex-col justify-center items-start sm:items-start sm:text-left px-4 sm:px-6 md:px-20">
+        <div className="w-[60%]">
           <h1 className="text-2xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-gray-800 mb-4 w-full sm:w-[70%] md:w-[60%] leading-snug">
             Empowering <br /> Hygiene & Comfort
           </h1>
-          <p className="text-lg w-[70%] sm:text-xl md:text-2xl text-gray-700 mb-4 sm:mb-6 leading-relaxed">
+          <p className="text-lg w-full sm:w-[70%] sm:text-xl md:text-2xl text-gray-700 mb-4 sm:mb-6 leading-relaxed">
             One Disposable at a Time
           </p>
-          <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-600 w-[60%] sm:w-[70%] md:w-[50%] max-w-2xl mb-6 sm:mb-8 leading-relaxed">
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-600 w-full sm:w-[70%] md:w-[50%] max-w-2xl mb-6 sm:mb-8 leading-relaxed">
             Explore premium-quality disposable products designed for salons,
             clinics, and wellness centers.
           </p>
