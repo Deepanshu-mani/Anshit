76feb9b2c2b185276f6c5a15f25e523d18ef1365
Mani <deepanshukumar1542004@gmail.com>
2025-04-14 14:45:01 +0530

    update files

--- ajanta/src/app/sitemap.xml/route.ts
+++ ajanta/src/app/sitemap.xml/route.ts
@@ -1,6 +1,4 @@
-import { MetadataRoute } from "next";
-
-export async function GET(): Promise<MetadataRoute.Sitemap> {
+export async function GET() {
   const baseUrl = "https://www.ajantacorporateindustry.com";
 
   const hrefs = [
@@ -12,8 +10,23 @@ export async function GET(): Promise<MetadataRoute.Sitemap> {
 
   const currentDate = new Date().toISOString();
 
-  return hrefs.map((href) => ({
-    url: `${baseUrl}${href}`,
-    lastModified: currentDate,
-  }));
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${hrefs
+      .map(
+        (href) => `
+        <url>
+          <loc>${baseUrl}${href}</loc>
+          <lastmod>${currentDate}</lastmod>
+        </url>
+      `
+      )
+      .join("")}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
